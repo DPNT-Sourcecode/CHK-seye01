e166e35d8aa3fb6f8045b965ab10575b1a37561c
sbx_user1051 <sbx_user1051@169.254.54.77>
2023-08-04 09:26:03 +0000

    Fri Aug 04 09:26:03 UTC 2023

--- lib/solutions/CHL/checklite_solution.py
+++ lib/solutions/CHL/checklite_solution.py
@@ -1,9 +1,22 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+
+def skus_to_dict(skus):
+    """
+    Takes a string of skus and returns a dict of skus and their counts
+    """
+    sku_count = {"A": 0, "B": 0, "C": 0, "D": 0}
+    for sku in skus:
+        sku_count[sku] += 1
+    return sku_count
+
+
 def checklite(skus):
     """
     The main method for the checkout process
     Must take a str of items (skus) and return an int representing the total cost
     """
     raise NotImplementedError()
+
 
