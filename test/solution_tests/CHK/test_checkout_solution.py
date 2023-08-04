50a940e92971872a50bc4c6c788a004074839569
sbx_user1051 <sbx_user1051@169.254.54.77>
2023-08-04 10:41:33 +0000

    Fri Aug 04 10:41:33 UTC 2023

--- test/solution_tests/CHK/test_checkout_solution.py
+++ test/solution_tests/CHK/test_checkout_solution.py
@@ -28,8 +28,43 @@ def test_new_items() -> None:
 
 
 def test_checkout_invalid() -> None:
-    assert checkout("Z") == -1
     assert checkout(19) == -1
     assert checkout("A19") == -1
+
+
+def test_basic_data_entry() -> None:
+    """
+    Test that I entered the right value for each product
+    """
+    assert checkout("G") == 20
+    assert checkout("H") == 10
+    assert checkout("I") == 35
+    assert checkout("J") == 60
+    assert checkout("K") == 80
+    assert checkout("L") == 90
+    assert checkout("M") == 15
+    assert checkout("N") == 40
+    assert checkout("O") == 10
+    assert checkout("P") == 50
+    assert checkout("Q") == 30
+    assert checkout("R") == 50
+    assert checkout("S") == 30
+    assert checkout("T") == 20
+    assert checkout("U") == 40
+    assert checkout("V") == 50
+    assert checkout("W") == 20
+    assert checkout("X") == 90
+    assert checkout("Y") == 10
+    assert checkout("Z") == 50
+
+def test_basic_offers_are_correct() -> None:
+    """
+    Another Data entry test, but this time for the offers
+    """
+    assert checkout ("HHHHH") == 45
+    assert checkout ("HHHHHHHHHH") == 80
+    assert
+
+
 
 
